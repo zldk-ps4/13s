6b194e72fea48411c2524717e0502df3a2501b79
zldk-ps4 <yoomovy@gmail.com>
2026-09-22 00:19:36 +0100

    Add files via upload

--- slopkit/chain_poops.js
+++ slopkit/chain_poops.js
@@ -41,7 +41,7 @@ const STOP_BEFORE_DOUBLE = params.get("stop") === "beforedouble";
 function hostOk() {
     var m = document.getElementById("msgs");
     if (m) {
-        m.innerHTML = "GoldHEN v2.4b18.10 Loaded ...";
+        m.innerHTML = "GoldHEN Loaded Successfully";
     }
 }
 
